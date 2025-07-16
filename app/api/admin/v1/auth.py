476f08fcd7ba5b954ac8d4051da7a82d7d36432a
Yashdeep1608 <rjyashsainirock24@gmail.com>
2025-07-16 23:55:58 +0530

    Update Code for pyament and user auth flow

--- app/api/admin/v1/auth.py
+++ app/api/admin/v1/auth.py
@@ -27,6 +27,9 @@ def register_user(user: UserCreate, request: Request,db: Session = Depends(get_d
         if existing_user:
             return ResponseHandler.bad_request(message=translator.t("username_exists", lang))
         
+        if existing_user.phone_number == user.phone_number:
+            return ResponseHandler.bad_request(message=translator.t("phone_number_exists", lang))
+        
         if user.referral_code:
             referred_by_user = created_user.get_user_by_referral_code(db,user.referral_code)
             if not referred_by_user:
