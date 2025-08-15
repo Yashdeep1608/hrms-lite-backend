a7f39474bd69f2bd951b4893d2d9e027c5e7bf45
Yashdeep1608 <rjyashsainirock24@gmail.com>
2025-08-16 03:19:50 +0530

    Update code

--- app/api/admin/v1/auth.py
+++ app/api/admin/v1/auth.py
@@ -62,7 +62,7 @@ def send_otp(request: Request, payload: SendOtp, db: Session = Depends(get_db)):
 
         new_otp = crud_user.create_otp_for_user(db=db,otp_type=payload.otp_type,user=user,isd_code=payload.isd_code,phone_number=payload.phone_number)
         
-        sent_otp = gupshup.send_whatsapp_otp_gupshup(payload.isd_code,payload.phone_number,new_otp)
+        # sent_otp = gupshup.send_whatsapp_otp_gupshup(payload.isd_code,payload.phone_number,new_otp)
         crud_user.mark_otp_as_sent(db, new_otp)
         # if sent_otp:
         #     crud_user.mark_otp_as_sent(db, new_otp)
@@ -179,13 +179,13 @@ def forgot_password(request: Request,payload: ForgetPassword, db: Session = Depe
             return ResponseHandler.bad_request(message=translator.t("user_not_found", lang))
 
         new_otp = crud_user.create_otp_for_user(db,OtpTypeEnum.ForgetPassword, user)
-        sent_otp = gupshup.send_whatsapp_otp_gupshup(user.isd_code,user.phone_number,new_otp)
-        # crud_user.mark_otp_as_sent(db, new_otp)
-        if sent_otp:
-            crud_user.mark_otp_as_sent(db, new_otp)
-        else:
-            logger.error(f"Failed to send OTP via WhatsApp to {user.isd_code}{user.phone_number}")
-            return ResponseHandler.bad_request(message=translator.t("otp_sent_failed", lang),)
+        # sent_otp = gupshup.send_whatsapp_otp_gupshup(user.isd_code,user.phone_number,new_otp)
+        crud_user.mark_otp_as_sent(db, new_otp)
+        # if sent_otp:
+        #     crud_user.mark_otp_as_sent(db, new_otp)
+        # else:
+        #     logger.error(f"Failed to send OTP via WhatsApp to {user.isd_code}{user.phone_number}")
+        #     return ResponseHandler.bad_request(message=translator.t("otp_sent_failed", lang),)
         return ResponseHandler.success(message=translator.t("otp_sent", lang), data={"user_id": new_otp.user_id})
     except Exception as e:
         return ResponseHandler.bad_request(message=translator.t("forget_password_error",lang),error=str(e))
